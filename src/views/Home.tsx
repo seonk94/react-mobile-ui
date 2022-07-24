15f7142ba9b8661683afaeaf54da1864dce5ecdf
seonk94 <seonk94@naver.com>
2022-07-24 20:43:44 +0900

    feat: home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Stack } from '@mui/material';
+import { Button, Grid, Stack, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,20 +11,27 @@ function Home() {
     navigate(path);
   };
   return (
-    <div>
-      <Stack spacing={2} direction="column">
-        {routeMap.map((item) => (
-          <Button
-            fullWidth
-            key={item.name}
-            onClick={() => buttonClickHandler(item.path)}
-            variant="contained"
-          >
-            {item.name}
-          </Button>
-        ))}
-      </Stack>
-    </div>
+    <main>
+      <div style={{ marginBottom: '24px' }}>
+        <Typography variant="h2">React UI/UX Lab</Typography>
+      </div>
+      <Grid container justifyContent="center">
+        <Grid item xs={12} md={6}>
+          <Stack spacing={2} direction="column">
+            {routeMap.map((item) => (
+              <Button
+                fullWidth
+                key={item.name}
+                onClick={() => buttonClickHandler(item.path)}
+                variant="contained"
+              >
+                {item.name}
+              </Button>
+            ))}
+          </Stack>
+        </Grid>
+      </Grid>
+    </main>
   );
 }
 
